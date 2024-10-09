7fe2c9b217b254179084aa30febae4103e46eb14
Matias Gregorat <19mati3@gmail.com>
2024-10-09 12:34:15 -0300

    Added Unitary Testing 2 & 3 & 13.

--- src/main/java/ar/com/mercadolibre/socialmeli/dto/exception/ExceptionDTO.java
+++ src/main/java/ar/com/mercadolibre/socialmeli/dto/exception/ExceptionDTO.java
@@ -2,8 +2,10 @@ package ar.com.mercadolibre.socialmeli.dto.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ExceptionDTO {
     private String message;
