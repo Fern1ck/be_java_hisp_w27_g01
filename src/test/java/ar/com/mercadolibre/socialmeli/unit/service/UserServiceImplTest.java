6aa72a245e26282307ae60c02d919768af271095
stephaaniie <stephaanie.castillo@gmail.com>
2024-10-09 12:14:25 -0300

    add to import ExtendWith.

--- src/test/java/ar/com/mercadolibre/socialmeli/unit/service/UserServiceImplTest.java
+++ src/test/java/ar/com/mercadolibre/socialmeli/unit/service/UserServiceImplTest.java
@@ -29,6 +29,7 @@ import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.*;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
