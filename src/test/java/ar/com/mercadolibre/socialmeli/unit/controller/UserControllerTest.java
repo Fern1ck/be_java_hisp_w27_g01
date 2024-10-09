fe59f18cb277e9f7499430c9997c8c5041004a89
stephaaniie <stephaanie.castillo@gmail.com>
2024-10-09 11:50:49 -0300

    featureTest/T-0001 | Add test controller.

--- src/test/java/ar/com/mercadolibre/socialmeli/unit/controller/UserControllerTest.java
+++ src/test/java/ar/com/mercadolibre/socialmeli/unit/controller/UserControllerTest.java
@@ -1,13 +1,33 @@
 package ar.com.mercadolibre.socialmeli.unit.controller;
 
+import ar.com.mercadolibre.socialmeli.controller.ProductController;
 import ar.com.mercadolibre.socialmeli.controller.UserController;
+import ar.com.mercadolibre.socialmeli.dto.response.FollowersListResponseDTO;
+import ar.com.mercadolibre.socialmeli.dto.response.PostDetailsResponseDTO;
+import ar.com.mercadolibre.socialmeli.dto.response.ProductResponseDTO;
+import ar.com.mercadolibre.socialmeli.dto.response.UserOkResponseDTO;
+import ar.com.mercadolibre.socialmeli.entity.User;
+import ar.com.mercadolibre.socialmeli.exception.BadRequestException;
+import ar.com.mercadolibre.socialmeli.service.impl.ProductServiceImpl;
 import ar.com.mercadolibre.socialmeli.service.impl.UserServiceImpl;
+import ar.com.mercadolibre.socialmeli.util.UtilTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
@@ -18,5 +38,25 @@ public class UserControllerTest {
     @InjectMocks
     UserController userController;
 
+    private List<User> users;
 
+    @BeforeEach
+    void setUp() {
+        users = UtilTest.createUsersWithPosts();
+    }
+
+    @Test
+    @DisplayName("T-0001 - Success")
+    void followASpecificUserByIdTest() {
+        //Arrange
+        UserOkResponseDTO userOk = userService.followASpecificUserById(1,2);
+        when(userService.followASpecificUserById(1, 2)).thenReturn(userOk);
+
+        // Act
+        ResponseEntity<?> response = userController.followASpecificUserById(1, 2);
+        UserOkResponseDTO responseBody = (UserOkResponseDTO) response.getBody();
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
 }
--- src/test/java/ar/com/mercadolibre/socialmeli/unit/service/ProductServiceImplTest.java
+++ src/test/java/ar/com/mercadolibre/socialmeli/unit/service/ProductServiceImplTest.java
@@ -16,4 +16,6 @@ public class ProductServiceImplTest {
 
     @InjectMocks
     ProductServiceImpl productService;
+
+
 }
